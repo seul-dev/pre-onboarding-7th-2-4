45c6c2a1febddbb49c576c675e42423bf4c27a12
seul-dev <seul.dev@gmail.com>
2022-11-03 14:06:20 +0900

    fix: detail 페이지 오류 수정

--- src/Pages/CarDetail/CarDetail.tsx
+++ src/Pages/CarDetail/CarDetail.tsx
@@ -6,42 +6,50 @@ import HeaderBar from "@/Components/Layouts/HeaderBar";
 import useGetCarDetail from "../../lib/hooks/useGetCarDetail";
 import { useParams } from "react-router-dom";
 import MainLayout from "@/Components/Layouts/MainLayout";
-import { FUELTYPE, ICar, SEGMENT } from "../../lib/types/index";
+import { FUELTYPE, SEGMENT } from "../../lib/types/index";
 import { dateFormat } from "../../lib/util/dateFormat";
 import { amountFormat } from "@/lib/util/amountFormat";
 
 const CarDetail = () => {
   const { id } = useParams();
   const { carInfo, isLoading } = useGetCarDetail(Number(id));
-  const { attribute, amount, insurance, additionalProducts, startDate } =
-    carInfo as ICar;
-  const { brand, name, segment, fuelType, imageUrl } = attribute;
 
   return (
     <div>
       <HeaderBar title="차량상세" prevButton={true} />
       {isLoading && <MainLayout message="불러오는 중" />}
-      <CarProfile imgUrl={imageUrl} brand={brand} name={name} />
-      <ListItem>월 {amountFormat(amount)} 원</ListItem>
-      <ListHeader title="차량 정보" />
-      <ListItem name="차종">{SEGMENT[segment]}</ListItem>
-      <ListItem name="연료">{FUELTYPE[fuelType]}</ListItem>
-      <ListItem name="이용 가능일">{dateFormat(startDate)}</ListItem>
-      <ListHeader title="보험" />
-      {insurance &&
-        insurance.map((item) => (
-          <ListItem key={item.name} name={item.name}>
-            {item.description}
-          </ListItem>
-        ))}
 
-      <ListHeader title="추가상품" />
-      {additionalProducts &&
-        additionalProducts.map((item) => (
-          <ListItem key={item.name} name={item.name}>
-            월 {amountFormat(item.amount)} 원
+      {carInfo && (
+        <>
+          <CarProfile
+            imgUrl={carInfo?.attribute.imageUrl}
+            brand={carInfo?.attribute.brand}
+            name={carInfo?.attribute.name}
+          />
+          <ListItem>월 {amountFormat(carInfo?.amount)} 원</ListItem>
+          <ListHeader title="차량 정보" />
+          <ListItem name="차종">{SEGMENT[carInfo.attribute.segment]}</ListItem>
+          <ListItem name="연료">
+            {FUELTYPE[carInfo.attribute.fuelType]}
           </ListItem>
-        ))}
+          <ListItem name="이용 가능일">
+            {dateFormat(carInfo?.startDate)}
+          </ListItem>
+          <ListHeader title="보험" />
+          {carInfo.insurance?.map((item) => (
+            <ListItem key={item.name} name={item.name}>
+              {item.description}
+            </ListItem>
+          ))}
+
+          <ListHeader title="추가상품" />
+          {carInfo.additionalProducts?.map((item) => (
+            <ListItem key={item.name} name={item.name}>
+              월 {amountFormat(item.amount)} 원
+            </ListItem>
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/lib/hooks/useGetCarDetail.ts
+++ src/lib/hooks/useGetCarDetail.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getCarListDetail, queryKeys } from "../api/index";
 
 const useGetCarDetail = (id: number) => {
-  const { data, isLoading } = useQuery(queryKeys.carDetail, getCarListDetail, {
-    select: (carList) => carList.filter((car) => car.id === id)
-  });
-  const carInfo = data?.length === 1 ? data[0] : {};
+  const { data, isLoading } = useQuery(queryKeys.carDetail, getCarListDetail);
+
+  const carInfo = data?.find((car) => car.id === id);
+
   return { carInfo, isLoading };
 };
 
