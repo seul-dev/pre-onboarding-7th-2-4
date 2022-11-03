1c7f7f2a64203d8c4902f98971849e5c39f81328
seul-dev <seul.dev@gmail.com>
2022-11-03 14:44:30 +0900

    fix: opengraph 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import Router from "./Routes/Router";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <Helmet>
+        <title>알티모빌리티</title>
+      </Helmet>
+    </>
+  );
 }
 
 export default App;
--- src/Components/OpenGraph/DetailOG.tsx
+++ src/Components/OpenGraph/DetailOG.tsx
@@ -13,12 +13,20 @@ const DetailOG = ({ brand, name, amount, imageUrl }: Props) => {
   return (
     <Helmet>
       <meta property="og:type" content="website" />
+      <meta
+        property="og:site_name"
+        content="알티모빌리티 B2C 차량대여 서비스"
+      />
+      <meta property="og:locale" content="en_US" />
       <meta property="og:title" content={`${brand} ${name}`} />
       <meta
         property="og:description"
         content={`월 ${amountFormat(amount)} 원`}
       />
+      <meta property="og:url" content={location.href} />
       <meta property="og:image" content={imageUrl} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Helmet>
   );
 };
